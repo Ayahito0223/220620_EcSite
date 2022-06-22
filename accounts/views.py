6e179c80424f789553658f5cff6333fd4f38658c
Ayahito0223 <Engineer.FumiM@gmail.com>
2022-06-22 23:24:22 +0900

    カスタムユーザー認証実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from accounts.models import CustomUser
-from accounts.forms import ProfileForm
+from accounts.forms import ProfileForm, SignupUserForm
+from allauth.account import views
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ProfileView(View):
+class ProfileView(LoginRequiredMixin, View):
   def get(self, request, *args, **kwargs):
     user_data = CustomUser.objects.get(id=request.user.id)
 
@@ -12,7 +14,7 @@ class ProfileView(View):
       'user_data': user_data,
     })
 
-class ProfileEditView(View):
+class ProfileEditView(LoginRequiredMixin, View):
   def get(self, request, *args, **kwargs):
     user_data = CustomUser.objects.get(id=request.user.id)
     form = ProfileForm(
@@ -44,3 +46,19 @@ class ProfileEditView(View):
     return render(request, 'accounts/profile.html', {
       'user_data': original_user_data
     })
+
+class LoginView(views.LoginView):
+  template_name = 'accounts/login.html'
+
+class LogoutView(views.LogoutView):
+  template_name = 'accounts/logout.html'
+
+  def post(self, *args, **kwargs):
+    if 'logout' in self.request.POST:
+      if self.request.user.is_authenticated:
+        self.logout()
+    return redirect('/')
+
+class SignupView(views.SignupView):
+  template_name = 'accounts/signup.html'
+  form_class = SignupUserForm
