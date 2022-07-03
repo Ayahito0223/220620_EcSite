8beed3a497b597814306cba1be6daebe4b067d0d
Ayahito0223 <Engineer.FumiM@gmail.com>
2022-07-03 15:33:58 +0900

    カート画面の実装まで

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import  MinValueValidator
+from django.conf import settings
 import random, string
 import hashlib
 
@@ -8,6 +9,10 @@ def slug_maker():
   return hashlib.md5(str.encode('utf-8')).hexdigest()
 
 class Item(models.Model):
+  """
+  商品の情報に関する事
+  """
+
   number = models.AutoField(primary_key=True)
   slug = models.SlugField(max_length=64, unique=True, default=slug_maker)
   code = models.CharField(max_length=7, null=False)
@@ -22,3 +27,40 @@ class Item(models.Model):
 
   def __str__(self):
     return self.city
+
+class OrderItem(models.Model):
+  """
+  一つの商品のオーダー状況に関する事
+  """
+
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  ordered = models.BooleanField(default=False)
+  item = models.ForeignKey(Item, on_delete=models.CASCADE)
+  quantity = models.IntegerField(default = 1)
+
+  def get_total_item_price(self):
+    return self.quantity * self.item.price
+
+  def __str__(self):
+    return f'{self.item.title}:{self.quantity}'
+  
+
+class Order(models.Model):
+  """
+  複数のオーダー状況に関する事
+  """
+
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  items = models.ManyToManyField(OrderItem)
+  start_date = models.DateTimeField(auto_now_add=True)
+  ordered_data = models.DateTimeField()
+  ordered = models.BooleanField(default=False)
+
+  def get_total(self):
+    total = 0
+    for order_item in self.items.all():
+      total += order_item.get_total_item_price()
+    return total
+
+  def __str__(self):
+    return self.user.email
--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from app import views
 
 urlpatterns = [
   path('', views.IndexView.as_view(), name="index"),
-  path('detail/<slug>', views.ItemDetailView.as_view(), name='detail')
+  path('detail/<slug>', views.ItemDetailView.as_view(), name='detail'),
+  path('additem/<slug>', views.addItem, name='additem'),
+  path('order/', views.OrderView.as_view(), name='order'),
 ]
