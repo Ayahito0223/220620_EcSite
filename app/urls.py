2b13f2ab6281888e963b804833d0b2603586fcc9
Ayahito0223 <Engineer.FumiM@gmail.com>
2022-07-03 20:50:33 +0900

    payment.htmlのコード書くまで

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
   path('detail/<slug>', views.ItemDetailView.as_view(), name='detail'),
   path('additem/<slug>', views.addItem, name='additem'),
   path('order/', views.OrderView.as_view(), name='order'),
+  path('removeitem/<slug>', views.removeItem, name='removeitem'),
+  path('removesingleitem/<slug>', views.removeSingleItem, name='removesingleitem'),
+  path('payment/', views.PaymentView.as_view(), name='payment'),
 ]
